36a9c07fabb08d8b68f5fa06b5c1d3e719bdfad7
huynhchin88 <huynhchin88@gmail.com>
2025-05-20 13:05:07 -1000

    added: update and delete functionalities

--- backend/controllers/blogController.js
+++ backend/controllers/blogController.js
@@ -1,15 +1,48 @@
 const Blog = require('../models/Blog');
 
 const getBlogs = async (req, res) => {
-    const blogs = await Blog.find().sort({ createdAt: -1 });
-    res.json(blogs);
+    try {
+        const blogs = await Blog.find().sort({ createdAt: -1 });
+        res.json(blogs);
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
 };
 
 const createBlog = async (req, res) => {
-    const { title, content } = req.body;
-    const newBlog = new Blog({ title, content });
-    await newBlog.save();
-    res.status(201).json(newBlog);
+    try {
+        const { title, content } = req.body;
+        const newBlog = new Blog({ title, content });
+        await newBlog.save();
+        res.status(201).json(newBlog);
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
 };
 
-module.exports = { getBlogs, createBlog }
+const updateBlog = async (req, res) => {
+    const { id } = req.params;
+    const { title, content } = req.body;
+    try {
+        const updatedBlog = await Blog.findByIdAndUpdate(
+            id, { title, content }, { new: true }
+        );
+        if (!updatedBlog) return res.status(404).json({ error: `Blog ID ${id} not found.` });
+        res.json(updatedBlog);
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
+};
+
+const deleteBlog = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const deletedBlog = await Blog.findByIdAndDelete(id);
+        if (!deletedBlog) return res.status(404).json({ error: `Blog ID ${id} don't exist.` });
+        res.json({ message: `Blog {$id} deleted successfully.` });
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
+};
+
+module.exports = { getBlogs, createBlog, updateBlog, deleteBlog }
--- backend/routes/blogRoutes.js
+++ backend/routes/blogRoutes.js
@@ -1,7 +1,9 @@
 const express = require('express');
-const { getBlogs, createBlog } = require('../controllers/blogController');
+const { getBlogs, createBlog, updateBlog, deleteBlog } = require('../controllers/blogController');
 const router = express.Router();
 
 router.get('/', getBlogs);
 router.post('/', createBlog);
+router.put('/:id', updateBlog);
+router.delete('/:id', deleteBlog);
 module.exports = router;
